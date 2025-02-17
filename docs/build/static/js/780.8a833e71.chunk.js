e73c3e944d88ec6708c6d7e3506398bda476a424
mykolapadii <mykola.padii@gmail.com>
2025-02-17 13:23:11 +0200

    update

--- docs/build/static/js/780.8a833e71.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksmart_reserve=self.webpackChunksmart_reserve||[]).push([[780],{780:(e,s,r)=>{r.r(s),r.d(s,{default:()=>l});var n=r(43),a=r(117),t=r(579);const c=()=>(0,t.jsx)(a.es,{children:(0,t.jsx)(a.b7,{name:"App",children:(0,t.jsx)(a.uA,{name:"Loader",children:(0,t.jsx)(a.KT,{children:(0,t.jsx)(d,{})})})})});function d(){return(0,t.jsx)(n.Fragment,{children:"Loading..."})}const l=()=>(0,t.jsx)(a.m8,{palette:(0,t.jsx)(c,{})})}}]);
-//# sourceMappingURL=780.8a833e71.chunk.js.map

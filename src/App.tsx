9aa4398cd0d38f4d699d40515fa25a23a9d0f178
mykolapadii <mykola.padii@gmail.com>
2025-02-17 12:23:25 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Smart Reserve</p>
       </header>
     </div>
   );
